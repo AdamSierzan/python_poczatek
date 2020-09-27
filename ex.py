2690da52e8856c1e75e10f734e17b136660b041e
adagioo1993 <adagioo1993@gmail.com>
2020-09-27 12:59:45 +0200

    daily update

--- ex.py
+++ ex.py
@@ -1,7 +1,2 @@
-import random
-expenditures = {}
-expenditures_names = ["woda", "gaz", "prad"]
+import Pygame
 
-for expenditure in expenditures_names:
-    expenditures[expenditure] = random.randint(1,100)
-print(expenditures)
